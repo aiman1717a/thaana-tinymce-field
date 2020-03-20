7bc00d355c4079632408312374e2790eba999ed6
aiman1717a <aiman1717a@gmail.com>
2020-03-21 02:28:59 +0500

    again

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,5 @@
 Nova.booting((Vue, router, store) => {
   Vue.component('index-thaana-tinymce-field', require('./components/IndexField'))
-  Vue.component('index-thaana-tinymce-field', require('./components/DetailField'))
-  Vue.component('index-thaana-tinymce-field', require('./components/FormField'))
+  Vue.component('detail-thaana-tinymce-field', require('./components/DetailField'))
+  Vue.component('form-thaana-tinymce-field', require('./components/FormField'))
 })
